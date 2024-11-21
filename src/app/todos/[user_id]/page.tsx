f47104b9dfd41fa23772ba679e81064a8865b34c
chiragmctrl <chirag@missionctrl.dev>
2024-11-21 17:13:16 +0530

    fix: Issue Solved

--- src/app/todos/[user_id]/page.tsx
+++ src/app/todos/[user_id]/page.tsx
@@ -1,10 +1,50 @@
-import { api } from "~/trpc/react";
+// "use client";
+// import React from "react";
+// import { api } from "~/trpc/react";
 
-export default function page({ params }: { params: { user_id: number } }) {
-  const user_id = params.user_id;
-  const todos = api.todo.getTodos.useQuery({ user_id: user_id });
+// export default function Page({
+//   params,
+// }: {
+//   params: Promise<{ user_id: string }>;
+// }) {
+//   const { user_id } = React.use(params); // Unwrap params with React.use()
+//   const userId = Number(user_id); // Convert to number if necessary
 
-  console.log("todos", todos);
+//   console.log("user_id", userId);
 
-  return <>{user_id}</>;
+//   const todos = api.post.hello.useQuery({ text: "Jp" });
+
+//   console.log("todos", todos);
+
+//   return <>{userId}</>;
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+// "use client";
+
+// import { api } from "~/trpc/react";
+// import React from "react";
+
+// export default function Page({
+//   params,
+// }: {
+//   params: Promise<{ user_id: string }>;
+// }) {
+//   const user_id = React.use(params).user_id;
+//   const todos = api.post.hello.useQuery({ text: user_id });
+
+//   console.log("todos", todos);
+//   return <div>My Post: {todos.data?.greeting}</div>;
+// }
+
+// -------------------------------------------------------------------------------------------------------------------------
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ user_id: string }>;
+}) {
+  const user_id = (await params).user_id;
+  return <div>My Post: {user_id}</div>;
 }
